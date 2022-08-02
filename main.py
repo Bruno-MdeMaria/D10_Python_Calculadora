db71a23c719c1f9658100891aa1b7dc3ff5dd3e7
Bruno-MdeMaria <brunomartinsdemaria@gmail.com>
2022-08-01 23:33:32 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+def format_name(f_name, l_name):
+    formato_first = (f_name.title()) #TITLE TRANFORMA TUDO PARA A PRIMEIRA LETRA MAIUSCULA E AS DEMAIS NINUSCULAS.
+    formato_last = (l_name.title())
+    return f"{formato_first} {formato_last}"
+
+print(format_name("bruno", "marTins")) #CHAMANDO A FUNÇÃO JUNTO COM O PRINT DA MESMA
+
+
+
+    
